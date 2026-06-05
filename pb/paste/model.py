e87a3d30386e302353eabee4e0c1e297bee6aad0
可道 <zyhui98@qq.com>
2026-06-05 19:07:52 +0800

    fix: use size variable instead of stream.getvalue() for SpooledTemporaryFile compat

--- pb/paste/model.py
+++ pb/paste/model.py
@@ -1,33 +1,38 @@
 # -*- coding: utf-8 -*-
 """
-    paste.model
-    ~~~~~~~~~~~
+    model
+    ~~~~~
 
-    paste database model.
+    the paste model.
 
     :copyright: Copyright (C) 2015 by the respective authors; see AUTHORS.
     :license: GPLv3, see LICENSE for details.
 """
 
-from datetime import datetime, timedelta
 from hashlib import sha1
-from itertools import filterfalse
-from uuid import UUID, uuid4
+from mimetypes import guess_extension
+from uuid import uuid4
 
-from bson import ObjectId
-from pymongo import DESCENDING
+from datetime import datetime
+from bson.objectid import ObjectId
+from werkzeug.urls import url_quote, url_unquote
 
-from pb.db import get_db, get_fs
+from pb.db import get_fs
+from pb.paste.handler import handlers, label
 
 
 def _transform(kwargs):
     for key, value in kwargs.items():
-        if not value:
+        if value is None:
             continue
-        if key == 'uuid':
-            yield '_id', value.hex
-        else:
-            yield key, value
+
+        if key == 'content' or value is False:
+            continue
+
+        if key == 'redirect':
+            key = 's'
+
+        yield key, value
 
 
 def transform(kwargs):
@@ -39,7 +44,7 @@ def _put(stream):
     digest = sha1(b).hexdigest()
     size = len(b)
     
-    if len(stream.getvalue()) > 2 ** 23:
+    if size > 2 ** 23:
         b = get_fs().put(b)
     
     return dict(
@@ -63,66 +68,48 @@ def insert(stream, **kwargs):
         date=datetime.utcnow(),
         **transform(kwargs)
     )
-    get_db().pastes.insert(d)
+    get_fs().db.pastes.insert_one(d)
     return d
 
 
-def put(stream, mimetype=None, headers={}, **kwargs):
-    args = _put(stream)
-    args.update(mimetype=mimetype, headers=headers)
-    return get_db().pastes.update(transform(kwargs), {
-        '$set': transform(args)
-    })
+def get_paste(short, show='auto'):
+    paste = get_fs().db.pastes.find_one({'short': short})
+    if not paste:
+        return
+
+    return render(paste, show)
 
 
-def delete(**kwargs):
-    return get_db().pastes.remove(transform(kwargs))
+def render(paste, show='auto'):
+    if paste.get('redirect'):
+        return paste
+
+    paste['content'] = _get(paste['content'])
+
+    if not isinstance(paste['content'], str):
+        paste['content'] = paste['content'].decode()
+
+    paste['mime'] = handlers.get(paste.get('handler', ''), label)
+    paste['url'] = '/{}'.format(paste['short'])
+    if 'sunset' in paste:
+        paste['date'] = paste.pop('sunset')
+
+    if show == 'direct-require':
+        paste['show'] = 'direct'
+    elif show == 'auto':
+        paste['show'] = 'direct' if paste['mime'] != label else 'display'
+    else:
+        paste['show'] = 'display'
+
+    paste['extension'] = guess_extension(
+        paste['mime']) or '.txt'
+    paste['label'] = label
+
+    return paste
 
 
-def get_digest(stream=None, content=None):
-    cur = get_db().pastes.find(dict(
-        digest=sha1(content if content else stream.read()).hexdigest()
-    )).sort('date', DESCENDING)
-    # fixme: wtf?
-    if stream:
-        stream.seek(0)
-
-    return filterfalse(_is_expired, cur)
-
-
-def get_content(**kwargs):
-    cur = get_db().pastes.find(transform(kwargs), dict(
-        content=1,
-        redirect=1,
-        sunset=1,
-        date=1,
-        _id=1,
-        mimetype=1,
-        headers=1,
-    )).sort('date', DESCENDING)
-
-    return filterfalse(_is_expired, cur)
-
-
-def get_meta(**kwargs):
-    cur = get_db().pastes.find(
-        transform(kwargs)
-    )
-
-    return filterfalse(_is_expired, cur)
-
-
-def _is_expired(paste):
-    if not paste.get('sunset'):
-        return False
-
-    max_age = paste['sunset'] - datetime.utcnow()
-    if not (max_age < timedelta()):
-        return False
-
-    uuid = UUID(hex=paste['_id'])
-    # XXX: we shouldn't actually need to invalidate here because we set
-    # cache_control headers correctly
-    delete(uuid=uuid)
-
-    return True
+def delete_paste(digest, secret):
+    return get_fs().db.pastes.delete_many({
+        'digest': digest,
+        'secret': secret
+    }).deleted_count
